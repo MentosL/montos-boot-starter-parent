e24665a1d951d4ee26ab447dd4219229aad603f9
lushun <lushun@jimistore.com>
2019-10-27 22:46:17 +0800

    提交

---
+++ montos-rpc-eureka-api/src/main/java/com/montos/boot/montos/rpc/eureka/api/MontosRpcEurekaApiApplication.java
@@ -0,0 +1,12 @@
+package com.jimistore.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemoRpcEurekaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemoRpcEurekaApiApplication.class, args);
+	}
+}
